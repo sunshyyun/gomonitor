e967c80e368afda0f928aac42c76a69042e450f3
李汉光 <lihanguang@xinyu668.com>
2023-03-08 09:57:04 +0800

    mod

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 go版本1.16.5
 
 ### 生成so文件
-go build -ldflags -pluginpath=monitor -buildmode=plugin -o monitor.so monitor.go
+go build -buildmode=plugin -o monitor.so main.go
--- buildSo.sh
+++ buildSo.sh
@@ -2,6 +2,5 @@
 
 echo "building monitor.so ..."
 
-go build -ldflags "-pluginpath=monitor" -buildmode=plugin -o monitor.so main.go
-
 go build -buildmode=plugin -o monitor.so main.go
+
